725d2236c3b9b0a4c64aa7f61de39f4e4c2b81f0
baiaiKouun <126437045+baiaiKouun@users.noreply.github.com>
2026-09-21 14:55:32 +0800

    配置错误修正

--- backend/main.py
+++ backend/main.py
@@ -19,7 +19,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=ALLOWED_ORIGINS,
     allow_methods=["GET", "POST"],
     allow_headers=["*"],
     allow_credentials=True,
